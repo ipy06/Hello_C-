99385acacb2ec83b0fefc7a31a9e649a1cd04f09
Zeke <Ipinoluwaheze@gmail.com>
2024-01-13 17:36:29 -0500

    added Strings.cpp

---
+++ Strings.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main (){
+	
+	string Text1 = "Ipy is" ;
+	string Text2 = " high 7" ;
+	cout << Text1 << Text2 << endl ;
+	string Text3 = " and 7 high is Ipy" ;
+	string Text = Text1 + Text2 + Text3 ; //Concatenation of Strings
+	cout << Text << endl ;
+	
+	
+	
+	//INPUT strings
+	string hdyft ; //How Do You Feel Today
+	cout << "How do you feel today\n"  ;
+	cin >> hdyft  ;
+	cout <<" After hearing your reply (" << hdyft <<"),\n I think you are set."; 
+}
