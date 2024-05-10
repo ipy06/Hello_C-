3f8fe6eba41f2c03ed47ba744c02905ee527629d
Zeke <Ipinoluwaheze@gmail.com>
2024-05-10 11:12:17 -0500

    Edit pointer.cpp code alignment and included header comment

--- pointer.cpp
+++ pointer.cpp
@@ -1,16 +1,22 @@
 #include<iostream>
+
+// This is a program illustrating the use of pointers.
+
+
 using namespace std;
-//Task 1 :
+
 int main(){
 	int v;
-	int u=3;
+	int u = 3;
 	int *pu, *pv; //pointer to an integer//
-	pu= &u; //assigns the address of u to pu//
-	v= *pu; //assigns the value of u to v//
-	pv= &v; //assigns the address of v to p//
+	pu = &u; //assigns the address of u to pu//
+	v = *pu; //assigns the value of u to v//
+	pv = &v; //assigns the address of v to p//
 	
-	cout<<endl<< "u = "<< u << "\n&u = "<< &u << "\npu = "<< pu << "\n*pu = "<<*pu;
-	cout<<"\nv = "<< v << "\n&v = "<< &v << "\npv = "<< pv << "\n*pv = "<< *pv;
+	cout << endl << "u = " << u << "\n&u = " << &u << "\npu = " 
+		<< pu << "\n*pu = "<<*pu;
+	cout << "\nv = " << v << "\n&v = " << &v << "\npv = " << pv 
+		<< "\n*pv = "<< *pv;
 	
 	return 0;
 }
